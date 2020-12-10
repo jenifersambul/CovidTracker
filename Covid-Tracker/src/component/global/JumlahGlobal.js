cf2fe216a4b9632a1dae6fdbdad3f283989bc6ac
jenifersambul <63536411+jenifersambul@users.noreply.github.com>
2020-12-10 22:40:52 +0800

    Update JumlahGlobal.js

--- Covid-Tracker/src/component/global/JumlahGlobal.js
+++ Covid-Tracker/src/component/global/JumlahGlobal.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState } from 'react';
 import axios from "axios"
-import "bootstrap/dist/css/bootstrap.min.css";
 import NumberFormat from "react-number-format";
 
 const JumlahGlobal = () => {
